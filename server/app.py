a0e3f66da7aabac559cc02846d9d73deb711b9c2
miyan1221 <103588276+miyan1221@users.noreply.github.com>
2023-05-22 17:15:29 +0800

    Update app.py
    服务端尝试解决跨域

--- server/app.py
+++ server/app.py
@@ -202,7 +202,8 @@ def upload_file():
 #     return jsonify(token_usage), 200
     response = jsonify(token_usage)
     response.headers.add('Access-Control-Allow-Origin', 'https://docs.sundayplus.xyz,http://docs.sundayplus.xyz')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response, 200
 
 
